e18ee5d11e4a3045b5c810cb1cff408b09a7d125
َAdel J Ahmadi <adel79.jalalahmadi@gmail.com>
2026-06-01 16:55:12 +0330

    feat: initial project structure with postgres and env security

---
+++ manage.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import os
+import sys
+from pathlib import Path
+import dotenv
+
+def main():
+    base_dir = Path(__file__).resolve().parent
+    env_path = base_dir / '.env'
+
+    if env_path.exists():
+        dotenv.read_dotenv(str(env_path))
+
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecom_project.settings')
+
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
---
+++ product/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+class Category(models.TextChoices):
+        ELECTRONIC = "Electronic"
+        LAPTOP = "Laptop"
+        ART = "Art"
+        FOOD = "Food"
+        HOME = "Home"
+        KITCHEN = "Kitchen"
+# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=20, default="" , blank=False)
+    description = models.TextField(max_length=1000, default="" , blank=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2 , default=0)
+    brand = models.CharField(max_length=20, default="" , blank=False)
+    category = models.CharField(max_length=20, default="" , blank=False)
+    rating = models.DecimalField(max_digits=3, decimal_places=2 , default=0)
+    stock = models.PositiveIntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
