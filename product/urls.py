2ad3750c6c66b338c911b524e702a832d31b33c8
َAdel J Ahmadi <adel79.jalalahmadi@gmail.com>
2026-06-09 17:25:09 +0330

    feat: implement product serializer and detail API endpoint

--- product/urls.py
+++ product/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products/', views.get_products , name='products')
+    path('products/', views.get_products , name='products'),
+    path('products/<str:pk>/', views.get_product_details , name='product-details')
+
 ]
--- product/views.py
+++ product/views.py
@@ -1,12 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from product.models import Product
+from product.serializers import ProductSerializer
 
 
 # Create your views here.
 @api_view (['GET'])
 def get_products(request):
     products = Product.objects.all()
-    return Response({"Test": 'test'})
+    serializer = ProductSerializer(products, many=True)
+    return Response({"products": serializer.data})
+
+@api_view(['GET'])
+def get_product_details(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response({"product": serializer.data})
