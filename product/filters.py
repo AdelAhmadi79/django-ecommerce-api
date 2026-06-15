a810aa586f68355e7c3092e626f27ee697b61371
َAdel J Ahmadi <adel79.jalalahmadi@gmail.com>
2026-06-15 13:17:18 +0330

    feat: implement keyword filtering, price range, and pagination

--- product/filters.py
+++ product/filters.py
@@ -1,10 +1,14 @@
 
 from django_filters import rest_framework as filters
-
 from product.models import Product
 
 
 class ProductFilter(filters.FilterSet):
+
+    keyword = filters.CharFilter(field_name='name', lookup_expr='icontains')
+    min_price = filters.NumberFilter(field_name='price'or 0, lookup_expr='gte')
+    max_price = filters.NumberFilter(field_name='price', lookup_expr='lte')
+
     class Meta:
         model = Product
-        fields = ('category', 'brand')
+        fields = ('keyword','category', 'brand' , 'min_price', 'max_price')
--- product/views.py
+++ product/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import api_view
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 from product.filters import ProductFilter
@@ -8,14 +9,29 @@ from product.serializers import ProductSerializer
 
 
 # Create your views here.
-@api_view (['GET'])
+@api_view(['GET'])
 def get_products(request):
-    filterset = ProductFilter(request.GET, queryset=Product.objects.all().order_by('id'))
-    serializer = ProductSerializer(filterset.qs, many=True)
-    return Response({"products": serializer.data})
+    filterset = ProductFilter(request.GET ,
+                              queryset=Product.objects.all().order_by('id'))
+
+    #pagination
+    result_per_page = 2
+
+    paginator = PageNumberPagination()
+    paginator.page_size = result_per_page
+
+    queryset = paginator.paginate_queryset(filterset.qs , request)
+
+    serializer = ProductSerializer(queryset, many=True)
+
+    return paginator.get_paginated_response(serializer.data)
+
 
 @api_view(['GET'])
 def get_product_details(request, pk):
+
     product = get_object_or_404(Product, pk=pk)
+
     serializer = ProductSerializer(product, many=False)
+
     return Response({"product": serializer.data})
